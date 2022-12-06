14d8fff7fcc7a23b7bca56e20a14be7ee5e5feec
Simoon Champney <simoonchampney@pop-os.localdomain>
2022-12-06 16:49:57 -0500

    neew Kode

--- rio/ruun.py
+++ rio/ruun.py
@@ -10,14 +10,17 @@ class MyRobot(wpilib.TimedRobot):
         #self.robot_drive = wpilib.drive.DifferentialDrive(self.mt1, self.mt2)
         self.joy = wpilib.Joystick(0) # fb
 
-
     def teleopPeriodic(self):
-        if(self.joy.getX()):
-            self.mt1.set(self.fb.getX())
-            self.mt2.set(self.fb.getX())
-        if(self.joy.getZ()):
-            self.mt1.set(self.joy.getZ())
-            self.mt2.set(-self.joy.getZ())
+        damp = .9
+        if(self.joy.getZ()>.1 or self.joy.getZ()<-.1):
+            self.mt1.set(self.joy.getZ()*damp)
+            self.mt2.set(self.joy.getZ()*damp)
+        elif(self.joy.getX()>.1 or self.joy.getX()<-.1):
+            self.mt1.set(self.joy.getX()*damp)
+            self.mt2.set(-self.joy.getX()*damp)
+        else:
+            self.mt1.set(0)
+            self.mt2.set(0)
 
         #self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getX())
 
