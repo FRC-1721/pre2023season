1db1bbb454bfba33f87d890ae264a58b4858159c
kredcool <kredcool2007@outlook.com>
2022-12-22 16:26:56 -0500

    fixxed controls it works

--- rio/robot.py
+++ rio/robot.py
@@ -1,29 +1,30 @@
 import wpilib
-import wpilib.drive 
-from rev import CANSparkMax, CANSparkMaxLowLevel 
+import wpilib.drive
+from rev import CANSparkMax, CANSparkMaxLowLevel
+
 
 class MyRobot(wpilib.TimedRobot):
-
     def robotInit(self):
-        self.mt1 = CANSparkMax(1,CANSparkMaxLowLevel.MotorType.kBrushless)
-        self.mt2 = CANSparkMax(2,CANSparkMaxLowLevel.MotorType.kBrushless)
-        #self.robot_drive = wpilib.drive.DifferentialDrive(self.mt1, self.mt2)
-        self.joy = wpilib.Joystick(0) # fb
+        self.mt1 = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.mt2 = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
+        # self.robot_drive = wpilib.drive.DifferentialDrive(self.mt1, self.mt2)
+        self.joy = wpilib.Joystick(0)  # fb
 
     def teleopPeriodic(self):
-        damp = .1
-        square = .3
-        if(self.joy.getZ()>square or self.joy.getZ()<-square):
-            self.mt1.set(self.joy.getZ()*damp)
-            self.mt2.set(self.joy.getZ()*damp)
-        elif(self.joy.getY()>square or self.joy.getY()<-square):
-            self.mt1.set(self.joy.getY()*damp)
-            self.mt2.set(-self.joy.getY()*damp)
+        damp = 0.1
+        square = 0.3
+        if self.joy.getZ() > square or self.joy.getZ() < -square:
+            self.mt1.set(self.joy.getZ() * damp * -1)
+            self.mt2.set(self.joy.getZ() * damp * -1)
+        elif self.joy.getY() > square or self.joy.getY() < -square:
+            self.mt1.set(self.joy.getY() * damp * -1)
+            self.mt2.set(-self.joy.getY() * damp * -1)
         else:
             self.mt1.set(0)
             self.mt2.set(0)
 
-        #self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        # self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getX())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     wpilib.run(MyRobot)
